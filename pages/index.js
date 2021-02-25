3aa2571573e34e3a4691331a239f75033159aaaa
marcoxdd <marcoacomarella@gmail.com>
2021-02-25 05:16:12 -0300

    primeira pagina

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,15 @@
 import{ useState } from 'react';
 
 function home(){
+    const [contador, timer] = useState(0);
+
     return (
         <div>
-            <h1>Home</h1>
-            <button />
-            </div>
-    )
-}
-
-
-function contador() {
-   const [contador,setContador] = useState(1);
-
-   function adicionarContador() {
-       setContador(contador +  gi1);
-   }
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar Contador</button>
+            <h1>Pagina inutil</h1>
+            <p>You clicked {contador} times</p>
+            <button onClick={() => timer(contador + 1)}>Clieck me</button>
         </div>
-    )
+    );
 }
-
-
 
 export default home
