cb68df6280b76ea586d1e38b3e36f6163a821099
marcoxdd <marcoacomarella@gmail.com>
2021-02-25 16:52:53 -0300

    nova commit na home

--- pages/index.js
+++ pages/index.js
@@ -3,23 +3,9 @@ import{ useState } from 'react';
 function home() {
     return (
         <div>
-            <h1>Home</h1>
-             <Contador />
+            <h1>Pagina inicial</h1>
             </div>
     )
-}
-function Contador() {
-   const [contador,setContador] = useState(1);
-
-   function adicionarContador() {
-       setContador(contador +  1);
-   }
-return( 
-<div>
-    <div>{contador}</div>
-    <button onClick = {adicionarContador}  >Adicionar Contador</button>
-</div> 
-)
 }
 
 
