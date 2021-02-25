bcc20fbf5e04674bffcd2265e35597cf4bf36298
marcoxdd <marcoacomarella@gmail.com>
2021-02-25 16:48:02 -0300

    nova home

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,10 @@
 import{ useState } from 'react';
 
-script(data-ad-client="ca-pub-4490220948438663", async, src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js")
 
 function home() {
     return (
         <div>
             <h1>Home</h1>
-             <Contador />
             </div>
     )
 }
-function Contador() {
-   const [contador,setContador] = useState(1);
-
-   function adicionarContador() {
-       setContador(contador +  1);
-   }
-return( 
-<div>
-    <div>{contador}</div>
-    <button onClick = {adicionarContador}  >Adicionar Contador</button>
-</div> 
-)
-}
-
-
-
-export default home
