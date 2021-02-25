e1c3b1e7e8b9b8c7ea4401efd68e4b8c5eb0370d
marcoxdd <marcoacomarella@gmail.com>
2021-02-25 16:50:34 -0300

    nova home

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,27 @@
 import{ useState } from 'react';
 
-
 function home() {
     return (
         <div>
             <h1>Home</h1>
+             <Contador />
             </div>
     )
 }
+function Contador() {
+   const [contador,setContador] = useState(1);
+
+   function adicionarContador() {
+       setContador(contador +  1);
+   }
+return( 
+<div>
+    <div>{contador}</div>
+    <button onClick = {adicionarContador}  >Adicionar Contador</button>
+</div> 
+)
+}
+
+
+
+export default home
