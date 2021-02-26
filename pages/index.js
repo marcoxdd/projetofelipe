644fb40051fe4acafd6899a5f2f3d7c42872da6f
marcoxdd <marcoacomarella@gmail.com>
2021-02-26 03:21:48 -0300

    teste

--- pages/index.js
+++ pages/index.js
@@ -10,18 +10,26 @@ function home() {
     // hahaha
 }
 function Mais() {
-   const [contador,setContador] = useState(1);
+   const [contador,setContador] = useState(0);
    function adicionarContador() {
        setContador(contador +  1);
    }
    function diminuirContador() {
        setContador(contador - 1);
    }
+   function multiplicarContador(){
+       setContador(contador * 2);
+   }
+   function dividirContador(){
+       setContador(contador / 2)
+   }
 return( 
 <div>
     <div>{contador}</div>
     <button onClick = {adicionarContador}  >+1</button>
     <button onClick = {diminuirContador} > -1 </button>
+    <button onClick = {multiplicarContador} > x2 </button>
+    <button onClick = {dividirContador} > /2 </button>
 </div> 
 )
 }
